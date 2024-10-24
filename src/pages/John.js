b00182da9dcde6cdefbf703a9d7e3ae6a0a3eb76
JastinAG <101855805+JastinAG@users.noreply.github.com>
2024-10-24 05:14:56 +0300

    Update John.js

--- src/pages/John.js
+++ src/pages/John.js
@@ -5,7 +5,7 @@ function John() {
   const name = "John Wekesa";
   const position = "Senior Partner";
   const about =
-    "John is a noted thought leader and expert on engineering and construction law, tax law, employment and litigation and counselling. John is also a human rights defender and a trusted adviser on financial technology.";
+    "John is a noted thought leader and expert in engineering and construction law, tax law, employment and litigation. He is also a human rights defender and a trusted adviser on financial technology.";
   return (
     <div>
       <ProfilePage
