0cc5006d967a4cfb05c302462d027197158229fa
emmanuel <jumaemmanuel462@yahoo.com>
2025-02-17 21:15:05 +0300

    added personcardjs

--- src/components/People/PersonCard.js
+++ src/components/People/PersonCard.js
@@ -28,3 +28,14 @@ const PersonCard = ({
 };
 
 export default PersonCard;
+
+
+
+
+<div className={${isMenuOpen? 'flex' 'hidden'} w-full h-fit bg-yellow-500 p-4 absolute top-[72px] left-0} onClick={closeMenu}>
+<ul className='flex flex-col justify-center items-center gap-2 w-full"> {navItems.map(({ Link, path }) => (
+<Link key={path} className='text-black uppercase font-semibold cursor-pointer p-2 rounded-1g Ohover:bg-black hover: text-white w-full text-center' to [path] spy {true} offset=-100)>(Link)</Link>
+))}
+</ul>
+</div>
+
